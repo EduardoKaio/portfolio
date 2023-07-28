b3ad4f6ac8b69b7dd10e087a9153326798c1a0f0
kaio <kaioeduardooficial@gmail.com>
2023-07-27 23:19:52 -0300

    alteracoes feitas

--- js/resume.js
+++ js/resume.js
@@ -11,11 +11,26 @@ function animeScroll(){
     })
 }
 animeScroll();
+
+
 if(elementos.length) {
   window.addEventListener('scroll', function() {
       animeScroll();
   });
 }
+function typeWrite(elemento){
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = ' ';
+  textoArray.forEach(function(letra, i){   
+    
+  setTimeout(function(){
+      elemento.innerHTML += letra;
+  }, 75 * i)
+
+});
+}
+const titulo = document.querySelector('.efeito-mach');
+typeWrite(titulo);
 
 (function ($) {
   "use strict"; // Start of use strict
